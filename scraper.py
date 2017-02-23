0ece4a63843c057ceebd70756866b8e3afaae3cb
ramesh7128 <ramesh7128@gmail.com>
2017-02-23 08:52:41 +0530

    edited visibility settings for pyvirtualdisplay

--- scraper.py
+++ scraper.py
@@ -13,7 +13,7 @@ import redis
 import time
 
 r = redis.Redis(host='localhost', port=6379)
-display = Display(visible=1)
+display = Display(visible=0)
 display.start()
 prof = webdriver.FirefoxProfile()
 starttime=time.time()
