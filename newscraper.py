1c3c51cfddf603fd617681786ee1f06e14c57c33
ramesh7128 <ramesh7128@gmail.com>
2017-02-23 12:43:09 +0530

    newscraper

--- newscraper.py
+++ newscraper.py
@@ -2,9 +2,10 @@ from xvfbwrapper import Xvfb
 from selenium import webdriver
 vdisplay = Xvfb()
 vdisplay.start()
+prof = webdriver.FirefoxProfile()
 
 # launch stuff inside virtual display here
-browser = webdriver.PhantomJS()()
+browser = webdriver.Firefox(firefox_profile = prof)
 browser.get('http://www.google.com')
 print browser.title
 browser.quit()
